443b5a7ea97225fe37ca0ead8976226a92e4fb01
yjqiang <13307130285@fudan.edu.cn>
2018-07-18 17:23:49 +0800

    小黑屋宝箱绕过

--- Silver.py
+++ Silver.py
@@ -15,6 +15,21 @@ async def GetAward():
         time_end = temp['data']['time_end']
         json_response = await bilibili.get_silver(time_start, time_end)
         return json_response
+        
+async def GetAward_black():
+    temp = await bilibili.get_time_about_silver()
+    # print (temp['code'])    #宝箱领完返回的code为-10017
+    if temp['code'] == -10017:
+        printer.info(["# 今日宝箱领取完毕"])
+    else:
+        time_start = temp['data']['time_start']
+        time_end = temp['data']['time_end']
+        for i in range(50):
+            json_response = await bilibili.get_silver(time_start, time_end)
+            
+            if json_response['code'] != 400:
+                print('宝箱小黑屋的结果返回', json_response)
+                return json_response
     
 
 async def run():
@@ -27,8 +42,15 @@ async def run():
         elif not json_rsp['code']:
             printer.info(["# 打开了宝箱"])
         elif json_rsp['code'] == 400:
-            print('小黑屋, 6小时后重试')
-            await asyncio.sleep(21600)
+            print('小黑屋, 暴力测试中')
+            tasklist = []
+            for i in range(60):
+                task = asyncio.ensure_future(GetAward_black())
+                tasklist.append(task)
+            await asyncio.wait(tasklist, return_when=asyncio.FIRST_COMPLETED)
+            json_rsp = await GetAward()
+            sleeptime = 3 * 60 + 5
+            await asyncio.sleep(sleeptime)
         else:
             printer.info(["# 继续等待宝箱冷却..."])
             # 未来如果取消了这个东西就睡眠185s，否则精确睡眠
