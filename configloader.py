a869441db4bb857f7aea639ec2faff9c443e9b4d
yjqiang <13307130285@fudan.edu.cn>
2018-08-10 17:30:36 +0800

    .

--- configloader.py
+++ configloader.py
@@ -12,39 +12,6 @@ def hex_to_rgb_percent(hex_str):
 def rgb_to_percent(rgb_list):
     color = webcolors.rgb_to_rgb_percent(rgb_list)
     return [float(i.strip('%'))/100.0 for i in color]
-
-    
-def load_bilibili(file):
-    with open(file, encoding="utf-8") as f:
-        dic_bilibili = toml.load(f)
-    if not dic_bilibili['account']['username']:
-        username = input("# 输入帐号: ")
-        password = input("# 输入密码: ")
-        dic_bilibili['account']['username'] = username
-        dic_bilibili['account']['password'] = password
-        with open(file, 'w', encoding="utf-8") as f:
-            toml.dump(dic_bilibili, f)
-            
-    return dic_bilibili
-    
-    
-def load_color(file):
-    with open(file, encoding="utf-8") as f:
-        dic_color = toml.load(f)
-    for i in dic_color.values():
-        for j in i.keys():
-            if isinstance(i[j], str):
-                i[j] = hex_to_rgb_percent(i[j])
-            else:
-                i[j] = rgb_to_percent(i[j])
-                    
-    return dic_color
- 
-               
-def load_user(file):
-    with open(file, encoding="utf-8") as f:
-        dic_user = toml.load(f)
-    return dic_user
     
     
 class ConfigLoader():
@@ -55,15 +22,15 @@ class ConfigLoader():
         if not cls.instance:
             cls.instance = super(ConfigLoader, cls).__new__(cls)
             cls.instance.colorfile = colorfile
-            cls.instance.dic_color = load_color(colorfile)
+            cls.instance.dic_color = cls.instance.load_color()
             # print(cls.instance.dic_color)
             
             cls.instance.userfile = userfile
-            cls.instance.dic_user = load_user(userfile)
+            cls.instance.dic_user = cls.instance.load_user()
             # print(cls.instance.dic_user)
             
             cls.instance.bilibilifile = bilibilifile
-            cls.instance.dic_bilibili = load_bilibili(bilibilifile)
+            cls.instance.dic_bilibili = cls.instance.load_bilibili()
             # print(cls.instance.dic_bilibili)
             print("# 初始化完成")
         return cls.instance
@@ -75,6 +42,36 @@ class ConfigLoader():
             dic_bilibili['saved-session'][i] = dic[i]
         with open(self.bilibilifile, 'w', encoding="utf-8") as f:
             toml.dump(dic_bilibili, f)
+            
+    def load_bilibili(self):
+        with open(self.bilibilifile, encoding="utf-8") as f:
+            dic_bilibili = toml.load(f)
+        if not dic_bilibili['account']['username']:
+            username = input("# 输入帐号: ")
+            password = input("# 输入密码: ")
+            dic_bilibili['account']['username'] = username
+            dic_bilibili['account']['password'] = password
+            with open(self.bilibilifile, 'w', encoding="utf-8") as f:
+                toml.dump(dic_bilibili, f)
+                
+        return dic_bilibili
+                
+    def load_color(self):
+        with open(self.colorfile, encoding="utf-8") as f:
+            dic_color = toml.load(f)
+        for i in dic_color.values():
+            for j in i.keys():
+                if isinstance(i[j], str):
+                    i[j] = hex_to_rgb_percent(i[j])
+                else:
+                    i[j] = rgb_to_percent(i[j])
+                        
+        return dic_color
+                        
+    def load_user(self):
+        with open(self.userfile, encoding="utf-8") as f:
+            dic_user = toml.load(f)
+        return dic_user
         
         
 
